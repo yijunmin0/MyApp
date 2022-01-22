0424e80c06cecd952f092592811f7098a4acb9a1
imjmt <yijunmin@gmail.com>
2022-01-23 02:47:59 +0900

    defaultDesign 생성

---
+++ src/assets/defaultDesign.ts
@@ -0,0 +1,9 @@
+import {Colors} from 'react-native-paper';
+
+//DD는 Default Design를 뜻한다.
+export const DDPadding = 10;
+export const DDHorizontalPadding = 16;
+export const DDMargin = 4;
+export const DDGreyLight = Colors.grey200;
+export const DDGreyNormal = Colors.grey300;
+export const DDGreyDark = Colors.grey400;
