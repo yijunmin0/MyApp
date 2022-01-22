f8b672de17e3eca7893a0aa36bc3fbadcba898ea
imjmt <yijunmin@gmail.com>
2022-01-23 01:42:28 +0900

    프로젝트 생성

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+
+const App = function () {
+  return (
+    <SafeAreaView>
+      <View />
+    </SafeAreaView>
+  );
+};
+
+export default App;
