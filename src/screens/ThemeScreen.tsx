b39a56c8cd2ecffffa65c31c7fbcf9a242744b44
imjmt <yijunmin@gmail.com>
2022-01-24 12:30:43 +0900

    ThemeScreen 스크린생성

--- src/screens/ThemeScreen.tsx
+++ src/screens/ThemeScreen.tsx
@@ -1,31 +1,47 @@
 import React from 'react';
-import {Button} from 'react-native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import {useDispatch} from 'react-redux';
+import {Text} from '../assets/customComponents/Text';
 import {View} from '../assets/customComponents/View';
+import {DDMargin} from '../assets/defaultDesign';
 import {modeChange} from '../store/themeSlice';
 
 export const ThemeScreen = function () {
   const dispatch = useDispatch();
   return (
-    <View>
-      <Button
-        title="light"
-        onPress={() => {
-          dispatch(modeChange('light'));
-        }}
-      />
-      <Button
-        title="dark"
+    <View style={styles.view}>
+      <TouchableOpacity
+        style={styles.selectionBox}
         onPress={() => {
           dispatch(modeChange('dark'));
-        }}
-      />
-      <Button
-        title="app"
+        }}>
+        <View>
+          <Text sizeLevel={5}>dark</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.selectionBox}
+        onPress={() => {
+          dispatch(modeChange('light'));
+        }}>
+        <View>
+          <Text sizeLevel={5}>light</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.selectionBox}
         onPress={() => {
           dispatch(modeChange('device'));
-        }}
-      />
+        }}>
+        <View>
+          <Text sizeLevel={5}>device</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  view: {flex: 1},
+  selectionBox: {margin: DDMargin},
+});
